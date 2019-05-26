76afd7855726b351f2a2e41234d1d9f2d53d51c1
Davi <dpassosdeveloper@gmail.com>
2019-05-26 17:37:28 -0300

    Consegui resolver mais um char!!!

--- exerc-lista-1-8.c
+++ exerc-lista-1-8.c
@@ -3,18 +3,19 @@
 #include <stdbool.h>
 #include <string.h>
 
-void main () {
    int a;
    char b;
    float c;
+void main () {
+   
     printf("Digite um número inteiro: \n");
     scanf("%d",&a);
     printf("Digite um caractere: \n");
-    scanf( "%c",&b);
+    scanf( "\n%c",&b);
     printf("Digite um número racional: \n");
     scanf("%f",&c);
-    printf("%f",c);
-    printf("%c",b);
-    printf("%d",a);
+    printf("\n%f",c);
+    printf("\n%c",b);
+    printf("\n%d",a);
     printf("\n");
     }
