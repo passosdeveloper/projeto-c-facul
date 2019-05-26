de54693ef68b02dcb08e80dd659e59bb98570960
Davi <dpassosdeveloper@gmail.com>
2019-05-25 21:22:07 -0300

    preciso testar no windows

---
+++ exerc-lista-1-8.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+void main () {
+   int a;
+   char b;
+   float c;
+    printf("Digite um número inteiro: \n");
+    scanf("%d",&a);
+    printf("Digite um caractere: \n");
+    scanf( "%c",&b);
+    printf("Digite um número racional: \n");
+    scanf("%f",&c);
+    printf("%f",c);
+    printf("%c",b);
+    printf("%d",a);
+    printf("\n");
+    }
